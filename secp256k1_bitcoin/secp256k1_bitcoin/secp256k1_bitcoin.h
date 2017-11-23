7bf3a02eb55618560b038ca0f1a05aebfe12f2ff
pebble8888 <pebble8888@gmail.com>
2017-11-23 22:48:14 +0900

    xcode study

---
+++ secp256k1_bitcoin/secp256k1_bitcoin/secp256k1_bitcoin.h
@@ -0,0 +1,19 @@
+//
+//  secp256k1_bitcoin.h
+//  secp256k1_bitcoin
+//
+//  Created by pebble8888 on 2017/10/22.
+//  Copyright © 2017年 pebble8888. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for secp256k1_bitcoin.
+FOUNDATION_EXPORT double secp256k1_bitcoinVersionNumber;
+
+//! Project version string for secp256k1_bitcoin.
+FOUNDATION_EXPORT const unsigned char secp256k1_bitcoinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <secp256k1_bitcoin/PublicHeader.h>
+
+
